e8f28ad9d45631e9c7cc94053d6ed2fc36db51d9
U-CS.MUM.EDU\986298 <986298@CS_LAB115-107.cs.mum.edu>
2018-06-28 15:36:01 -0500

    Lab 4 - Second Exercise Done as well!

--- Lab4/Exercise1/readFileCustom.js
+++ Lab4/Exercise1/readFileCustom.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
 function readFile(resObj) {
-    console.log("2");
     var readStream = fs.createReadStream(__dirname + '/aimal.txt', 'utf-8');
     readStream.on('data', function(data){
         process.send(data);
--- Lab4/Exercise2/app.js
+++ Lab4/Exercise2/app.js
@@ -1,3 +1,38 @@
 var os = require("os");
 
-console.log();
+var checkMemPromise = function () {
+    return new Promise(function(resolve, reject){
+        var totalGBs = os.totalmem() / 1073741824;
+        if(totalGBs > 4){
+            resolve("success");
+        }
+        else {
+            reject("The processor Needs atleast 2GB of RAM!");
+        }
+    });
+}
+
+var checkCoresPromise = function () {
+    return new Promise(function(resolve, reject){
+        if(os.cpus().length >= 2){
+            resolve("success");
+        }
+        else {
+            reject("Processor not supported");
+        }
+    });
+}
+
+async function checkSystem(){
+    try {
+        console.log("Checking your system..");
+        await checkMemPromise();
+        await checkCoresPromise();
+        console.log("System is checked successfully!");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+checkSystem();
